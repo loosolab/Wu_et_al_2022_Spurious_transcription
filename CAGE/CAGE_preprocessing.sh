172ced7968debda7764f0f6ab4753036fd79a5be
ckuenne <ckuenne@users.noreply.github.com>
2022-10-20 16:39:30 +0200

    Update CAGE_preprocessing.sh

--- CAGE/CAGE_preprocessing.sh
+++ CAGE/CAGE_preprocessing.sh
@@ -3,7 +3,5 @@ yua35_Fan_Cage_seq_japan
 
 
 
-
-
-## Basic CAGE analysis was peformed by the RIKEN Institute Japan using the published RECLU pipeline (RECLU: a pipeline to discover reproducible transcriptional start sites and their alternative regulation using capped analysis of gene expression (CAGE), Ohmiya et. al., 2014)
+## Basic CAGE analysis was peformed by the RIKEN Institute Japan using the published RECLU pipeline (RECLU: a pipeline to discover reproducible transcriptional start sites and their alternative regulation using capped analysis of gene expression (CAGE), Ohmiya et. al., BMC Genomics. 2014 Apr 25;15:269. doi: 10.1186/1471-2164-15-269)
 ## code repository: https://osdn.net/projects/reclu/releases/
