03a6ac65a0f1ac132f596a31e696fc6af88ad648
ckuenne <ckuenne@users.noreply.github.com>
2022-10-24 10:42:52 +0200

    Update CAGE_base_processing.sh

--- CAGE/CAGE_base_processing.sh
+++ CAGE/CAGE_base_processing.sh
@@ -1,11 +1,5 @@
-REMOVE THIS LATER:
-yua35_Fan_Cage_seq_japan
-
-
-
 ## Basic CAGE analysis was peformed by the RIKEN Institute Japan using the published RECLU pipeline (RECLU: a pipeline to discover reproducible transcriptional start sites and their alternative regulation using capped analysis of gene expression (CAGE), Ohmiya et. al., BMC Genomics. 2014 Apr 25;15:269. doi: 10.1186/1471-2164-15-269)
 ## code repository: https://osdn.net/projects/reclu/releases/
-
 
 ##CAGE processing
 #acquired mapped bam files from RILKEN 
